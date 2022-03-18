50ddd6657d40d29fad1dab364b5f05a5d480069c
leanjp84 <lean84@gmail.com>
2022-03-18 09:44:32 +0100

    done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,86 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+<h1>IronContacts</h1>
+<button @click="addRandomContact">Add Random Contact</button>
+<button @click="sortByName">Sort by Name</button>
+<button @click="sortByPopularity">Sort by Popularity </button>
+<div class="tableStyle">
+<table>
+  <thead>
+    <tr>
+      <th><h3>Picture</h3></th>
+      <th><h3>Name</h3></th>
+      <th><h3>Popularity</h3></th>
+      <th><h3>Oscar</h3></th>
+      <th><h3>Emmy</h3></th>
+      <th><h3>Delete</h3></th>
+    </tr>
+  </thead>
+ <tbody>
+    <tr v-for="contact in displayContacts" :key="contact.id">
+        <td><img class="imageSize" v-bind:src="contact.pictureUrl"></td>
+        <td>{{contact.name}}</td>
+        <td>{{(contact.popularity).toFixed(2)}}</td>
+        <td>{{contact.wonOscar ? "🏆":""}}</td>
+        <td>{{contact.wonEmmy ? "🏆":""}}</td>
+        <td><button @click="deleteContact (contact.id)">Delete</button></td>
+      </tr>
+    </tbody>
+</table>
+</div>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
+<script setup>
+import {reactive} from 'vue'
+import contacts from "../contacts.json"
+
+  const myContacts = reactive (contacts);
+  //console.log(myContacts);
+  let displayContacts = reactive (contacts.slice(0, 5))
+
+
+function addRandomContact(){
+       if(contacts.length>0){
+      let newContact = contacts.splice(Math.floor(Math.random()*contacts.length-1),1);
+  displayContacts.push(newContact[0]);
+  }
+} 
+
+function sortByName(){
+    displayContacts.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+})
+}
+function sortByPopularity(){
+displayContacts.sort(function (a, b) {
+  if (a.popularity > b.popularity) {
+    return -1;
+  }
+  if (a.popularity < b.popularity) {
+    return 1;
+  }
+  return 0;
+})
+}
+function deleteContact(id){
+  let index = displayContacts.findIndex(contact => contact.id == id);
+  contacts.push(...displayContacts.splice(index,1))
+}
+
+
+</script>
+
+<style>
+.imageSize {
+  width: 50px;
+}
+.tableStyle {
+  display: flex;
+  justify-content: center;
 }
 </style>
